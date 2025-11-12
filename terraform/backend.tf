7c4b743880d2f6b1fc29f3c5ed4ce0adf766497d
unil mittakola <unil4869@gmail.com>
2025-11-12 17:01:49 +0530

    fresh

---
+++ terraform/backend.tf
@@ -0,0 +1,4 @@
+terraform { # Configure the Google Cloud Storage backend
+  backend "gcs" { # GCS backend configuration
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  description = "Name of the GCS bucket"
+  type        = string
+}
