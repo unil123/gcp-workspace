60715457dc5c3203c4990b6eff9cb5f356ab4837
Mittakola Unil <98331928+unil123@users.noreply.github.com>
2025-11-12 17:37:36 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,7 @@
-terraform {
-  backend "gcs" {
-    bucket  = "my-unique-bucket-name-123"
-    prefix  = "terraform/state"
-  }
-}
+#terraform {
+#  backend "gcs" {
+#   bucket  = "my-unique-bucket-name-123"
+#   prefix  = "terraform/state"
+# }
+#}
+
