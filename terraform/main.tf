a71c34b8522030edaa1cdc55f2523252ee0f8d85
Mittakola Unil <98331928+unil123@users.noreply.github.com>
2025-11-12 18:01:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,4 @@
 provider "google" {
   project = "hopeful-subject-475706-d5"
   region  = "us-central1"
-  credentials = null
-}
-
-resource "google_storage_bucket" "my_bucket" {
-  name     = "my-unique-bucket-name-123"
-  location = "US"
 }
